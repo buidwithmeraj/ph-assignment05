32bc98f9e303c08eb3858d353c59f73142b512f5
Meraj <buildwithmeraj@gmail.com>
2025-08-27 12:46:11 +0600

    Updated some texts, fixed some minor bugs

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function decreaseCount(element, decrement) {
     count.innerText = Math.max(0, parseInt(count.innerText) - decrement);
 }
 function copyContent(title, number) {
-    navigator.clipboard.writeText(title + ": " + number).then(function () {
+    navigator.clipboard.writeText(number).then(function () {
         let copyCount = getTextContent("copy");
         copyCount = increaseCount("copy", 1);
         alert('Copied ' + title + ": " + number);
