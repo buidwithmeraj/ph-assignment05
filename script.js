c4a8352f037ad3bf26f07ed05253fc5522c42acb
Meraj <buildwithmeraj@gmail.com>
2025-08-29 18:47:23 +0600

    added icons to alerts

--- script.js
+++ script.js
@@ -14,21 +14,21 @@ function copyContent(title, number) {
     navigator.clipboard.writeText(number).then(function () {
         let copyCount = getTextContent("copy");
         copyCount = increaseCount("copy", 1);
-        alert('Copied ' + title + ": " + number);
+        alert('✅ Copied ' + title + ": " + number);
     }, function (err) {
-        console.error('Could not copy text: ', err);
+        console.error('❌ Could not copy text: ', err);
     });
 }
 
 function callNumber(title, number) {
     let coins = getTextContent("coin");
     if (coins < 20) {
-        alert("Not enough coins. You need at least 20 coins to make a call.");
+        alert("❌ Not enough coins. You need at least 20 coins to make a call.");
         return;
     }
     decreaseCount("coin", 20);
     addCallHistory(title, number);
-    alert('Calling ' + title + ": " + number);
+    alert('📞 Calling ' + title + ": " + number + "...");
 
 }
 
@@ -57,7 +57,7 @@ function clearHistory() {
             historyList.removeChild(child);
         }
     }
-    alert("Call history cleared.");
+    alert("✅ Call history cleared.");
 }
 
 const hearts = document.querySelectorAll('.fa-heart');
